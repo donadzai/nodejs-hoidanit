e039b4ea5fe724f83b20d8b3e5ef25d9a021125d
donadzai <kieuduydoan2k2@gmail.com>
2024-01-11 14:24:38 +0700

    connect DB real

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,8 @@
-const home = (req, res) => {
-  res.render("home");
+const models = require('../models');
+
+const home = async (req, res) => {
+    const data = await models.User.findAll();
+    res.render('home', { data });
 };
 
 module.exports = { home };
--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
-const express = require("express");
-const { engine } = require("express-handlebars");
-const path = require("path");
+const express = require('express');
+const Handlebars = require('handlebars');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const connectDB = require('./config/connectDB');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
 const app = express();
-const routes = require("./route");
+const routes = require('./route');
 
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('.hbs', engine({ extname: '.hbs', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+connectDB();
 
 routes(app);
 
